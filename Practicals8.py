c59cafea6214027a9cda9da684d07747fe353387
black-ninjax <157094412+black-ninjax@users.noreply.github.com>
2024-01-23 02:40:01 +0530

    Create Practicals8.py
    Practical Title : A program that sorts a list of numbers in
    Ascending or Descending order.
    Practical Description: The objective of this practical is to
    develop a Python program that sorts a list of numbers in Ascending
    or Descending order. . This exercise will help students understand
    basic programming concepts input/output operations, lists, basic
    python methods like a.sort() and a.sort(reverse=True).

---
+++ Practicals8.py
@@ -0,0 +1,10 @@
+For Ascending order:
+abc=list(input("Enter Numbers into the list : "))
+print("Before sorting",abc)
+abc.sort()
+print("after sorting ",abc)
+For Descending order:
+abcd=list(input("Enter Numbers into the list : "))
+print("before sorting , a= ",abcd)
+abcd.sort(reverse=True)
+print("after sorting, a = ",abcd)
